ea15ece07491c3504f84698f288950c6d9a33221
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-21 11:57:13 -0500

    created database and able to SELECT all the tables within

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,27 @@
+DROP TABLE IF EXISTS department;
+
+DROP TABLE IF EXISTS role;
+
+DROP TABLE IF EXISTS employees;
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
 
-    name VARCHAR(30), --will hold department name
+    name VARCHAR(30),
 
     description VARCHAR(50)
 
-    --determine other properties of a department
-
-    --share a foreign key with employee probably
 );
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
 
-    title VARCHAR(30), --will hold role title
+    title VARCHAR(30),
 
     salary DECIMAL(10, 2),
 
-    department_id INTEGER, --holds ref. to department the role belongs to (foreign key)
+    department_id INTEGER
 
-    --decide which other properties, if any, the "role should have"
 );
 
 CREATE TABLE employees (
@@ -29,8 +31,7 @@ CREATE TABLE employees (
 
     last_name VARCHAR(30),
 
-    role_id INTEGER, --holds ref. to employee role
+    role_id INTEGER,
 
-    manager_id INTEGER, --holds ref. to another emp. that is the manager of the current employee
-    --manager_id can be NULL if employee has no manager
+    manager_id INTEGER
 );
