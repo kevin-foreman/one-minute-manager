d5b0a2dc2057b98bbad21526bc031761c1b23460
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-01 13:24:40 -0500

    Added console.table dependency to queries.js and added to first query

--- queries.js
+++ queries.js
@@ -1,4 +1,5 @@
 const db = require('./config/connection.js');
+const cTable = require('console.table');
 
 // create classes to hold various sections of the business
 // constructor function
@@ -30,9 +31,24 @@ const showAllRoles = function() {
 const showAllEmployees = function() {
     return await db.query(
 
-        `SELECT employee.id,
-        employee.first_name,
-        employee.last_name`
+        `SELECT E.id,
+        E.first_name,
+        E.last_name,
+        R.title,
+        D.name AS department,
+        R.salary,
+        FROM employee E,
+        JOIN role R ON E.role_id = R.id,
+        JOIN department D ON R.department_id = D.id,
+        LEFT OUTER JOIN employee M ON E.manager_id = M.id;`,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.table
+            
+            // return;
 
         // add JOIN statments to structure
     )
